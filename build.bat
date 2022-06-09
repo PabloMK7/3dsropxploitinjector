8b50b5336177c7aac41f201475bdf616d89f1cb8
zoogie <stackrestacker@gmail.com>
2022-06-09 05:32:59 -0500

    Fix gitignore and keep menu_ropbin.exe

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-python scripts/buildVersion.py "N11.15.0-47K" --enableotherapp
-rem python scripts/buildVersion.py "N11.15.0-47U" --enableotherapp
+python scripts/buildVersion.py "N11.15.0-47U" --enableotherapp
+rem python scripts/buildVersion.py "N11.15.0-47K" --enableotherapp
 rem python scripts/buildVersion.py "N11.15.0-47J" --enableotherapp
 rem python scripts/buildVersion.py "N11.15.0-47E" --enableotherapp
 pause
