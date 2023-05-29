4a9d0a12b40f0b209c9499728e01e9d8931d5d71
PabloMK7 <hackyglitch2@gmail.com>
2023-05-29 19:48:03 +0200

    11.17 and add menuhax67 notice

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 set PATH=%PATH%;C:\devkitPro\devkitARM\bin
-python scripts/buildVersion.py "11.15.0-47U" --enableotherapp
-python scripts/buildVersion.py "11.15.0-47E" --enableotherapp
-python scripts/buildVersion.py "11.15.0-47J" --enableotherapp
-python scripts/buildVersion.py "11.15.0-47K" --enableotherapp
+python scripts/buildVersion.py "11.17.0-50U" --enableotherapp
+python scripts/buildVersion.py "11.17.0-50E" --enableotherapp
+python scripts/buildVersion.py "11.17.0-50J" --enableotherapp
+python scripts/buildVersion.py "11.17.0-50K" --enableotherapp
 pause
